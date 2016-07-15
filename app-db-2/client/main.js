1db4f3803d9a8af0b13cc1202024583adde7b5bf
Mohit Sewak <Mohit Sewak>
2016-07-16 00:20:45 +0530

    edit

--- app-db-2/client/main.js
+++ app-db-2/client/main.js
@@ -3,23 +3,39 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+   Template.images.helpers({images:
+    Images.find({}, {sort:{createdOn: -1, rating:-1}})
+  });
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
-});
+   Template.images.events({
+    'click .js-image':function(event){
+        $(event.target).css("width", "50px");
+    }, 
+    'click .js-del-image':function(event){
+       var image_id = this._id;
+       console.log(image_id);
+       // use jquery to hide the image component
+       // then remove it at the end of the animation
+       $("#"+image_id).hide('slow', function(){
+        Images.remove({"_id":image_id});
+       })  
+    }, 
+    'click .js-rate-image':function(event){
+      var rating = $(event.currentTarget).data("userrating");
+      console.log(rating);
+      var image_id = this.id;
+      console.log(image_id);
+
+      Images.update({_id:image_id}, 
+                    {$set: {rating:rating}});
+    }, 
+    'click .js-show-image-form':function(event){
+      $("#image_add_form").modal('show');
+    }
+
+   });
+
 
   Template.image_add_form.events({
     'submit .js-add-image':function(event){
--- app-db-2/server/main.js
+++ app-db-2/server/main.js
@@ -1,5 +1,19 @@
 import { Meteor } from 'meteor/meteor';
 
+
+Images = new Mongo.Collection("images");
+console.log("Images in db before startup:"+Images.find().count());
+
 Meteor.startup(() => {
   // code to run on server at startup
+    if (Images.find().count()==0){
+        for (var i=1;i<23;i++){
+             Images.insert({
+                img_src:"img_"+i+".jpg", 
+                img_alt:"Image #:"+i, 
+                createdOn:new Date()
+      });
+        }
+    }
+console.log("Images in db after loop load:"+Images.find().count());
 });
