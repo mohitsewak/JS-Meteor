81392523a8e79f4e0206be9f137baeae46b9bbe8
Mohit Sewak <Mohit Sewak>
2016-07-24 22:58:49 +0530

    added functionalities to display existing comments

--- peer_assessment_siteace/siteace.js
+++ peer_assessment_siteace/siteace.js
@@ -106,17 +106,23 @@ if (Meteor.isClient) {
 			$("#website_form").toggle('slow');
 		}, 
 		"submit .js-save-website-form":function(event){
-
 			// here is an example of how to get the url out of the form:
 			var url = event.target.url.value;
 			console.log("The url they entered is: "+url);
-			
 			//  put your website saving code in here!	
-
 			return false;// stop the form submit from reloading the page
 
 		}
 	});
+    
+    Template.website.events({
+       'submit .js-comment-form':function(event){
+           console.log(event.currentTarget);
+           console.log(event.target);
+           console.log(this._id);
+           return false;
+       } 
+    });
 }
 
 
